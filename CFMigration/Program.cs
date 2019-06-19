02d989e7ea1eb8c0d45d394ee8e3500936457ed5
chuangmaster <masterchuang@urtody.com>
2019-06-20 01:41:33 +0800

    ObjectContext vs DbContext

--- CFMigration/Program.cs
+++ CFMigration/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CFMigration
@@ -10,36 +13,41 @@ namespace CFMigration
     {
         static void Main(string[] args)
         {
-            using (var model = new KTStoreModel())
+            using (var context = new KTStoreModel())
             {
-                model.Database.Log = Console.Write;
+                ObjectContextDemo(context);
 
-                ShowDBSetting(model);
-
-                //AddData(model);
-
-                var products = from p in model.Product select p;
-                foreach (var item in products)
-                {
-                    Console.WriteLine($"{item.Name}:{item.Price}");
-                }
-
-                //var product = model.Product.First();
-                //product.Name = "ASUS ROG";
-                //model.SaveChanges();
-                //model.Entry(product).Reload();
-                //product = model.Product.First();
-                //Console.WriteLine($"{product.Name}:{product.Price}");
-
-
+                DbContextDemo(context);
             }
 
             Console.Read();
         }
 
-        public static void AddData(KTStoreModel model)
+        public static void OldTest(KTStoreModel context)
         {
-            if (model.Product.Count() <= 0)
+            context.Database.Log = Console.Write;
+
+            ShowDBSetting(context);
+
+            //AddData(context);
+
+            var products = from p in context.Product select p;
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Name}:{item.Price}");
+            }
+
+            //var product = context.Product.First();
+            //product.Name = "ASUS ROG";
+            //context.SaveChanges();
+            //context.Entry(product).Reload();
+            //product = context.Product.First();
+            //Console.WriteLine($"{product.Name}:{product.Price}");
+        }
+
+        public static void AddData(KTStoreModel context)
+        {
+            if (context.Product.Count() <= 0)
             {
                 var data1 = new Product()
                 {
@@ -53,21 +61,45 @@ namespace CFMigration
                     Price = 60000M,
                     Quantity = 1
                 };
-                model.Product.Add(data1);
-                model.Product.Add(data2);
-                model.SaveChanges();
+                context.Product.Add(data1);
+                context.Product.Add(data2);
+                context.SaveChanges();
+            }
+        }
+
+        public static void ShowDBSetting(KTStoreModel context)
+        {
+            Console.WriteLine($@"
+                連接字串:{context.Database.Connection}
+                DB Name:{context.Database.Connection.Database}
+                ServerName Name:{context.Database.Connection.DataSource}
+                ConnectionState:{context.Database.Connection.State}
+            ");
+        }
+
+        public static void ObjectContextDemo(KTStoreModel context)
+        {
+            ObjectContext objectContext = (context as IObjectContextAdapter).ObjectContext;
+            ObjectSet<Product> products = objectContext.CreateObjectSet<Product>();
+            var oq = (ObjectQuery)products.Where(p => p.Price > 1000 && p.Price < 50000);
+            Console.WriteLine(oq.ToTraceString());
+            foreach (Product product in oq)
+            {
+                Console.WriteLine(product.Name);
+            }
+        }
+
+        public static void DbContextDemo(KTStoreModel context)
+        {
+
+            var oq = context.Product.Where(p => p.Price > 1000 && p.Price < 50000);
+            Console.WriteLine(oq.ToString());
+            foreach (Product product in oq)
+            {
+                Console.WriteLine(product.Name);
             }
         }
 
 
-        public static void ShowDBSetting(KTStoreModel model)
-        {
-            Console.WriteLine($@"
-                連接字串:{model.Database.Connection}
-                DB Name:{model.Database.Connection.Database}
-                ServerName Name:{model.Database.Connection.DataSource}
-                ConnectionState:{model.Database.Connection.State}
-            ");
-        }
     }
 }
